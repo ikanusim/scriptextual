d2b20bbe816c213cdb1689829cab6dda9dca9d3d
Daniel Hüsch <daniel.huesch@gmail.com>
2009-03-06 21:39:38 +0100

    - added jquery.sprintf - added copy of jquery itself as I develop offline most of the time recently - added functions for time tracking

--- scriptextual.js
+++ scriptextual.js
@@ -1,3 +1,19 @@
+var stx = {
+  i: function(s) { return parseInt(s.replace(/^0/, '')) },
+  duration: function(line) {
+    var parts = line.match(/(\d{2})(\d{2})-(\d{2})(\d{2})/);
+    var bounds = line.split('-');
+    var from = stx.i(parts[1]) * 60 + stx.i(parts[2]);
+    var to = stx.i(parts[3]) * 60 + stx.i(parts[4]);
+    if (to < from)
+      to += 1440;
+    return to - from
+  },
+  hhmm: function(m) {
+    return $.sprintf('%02d%02d', Math.floor(m / 60), m % 60);
+  }
+};
+
 var functions = {
   func_strip_whitespace_in_lines: {
     description: 'Remove leading and trailing whitespace in each line',
@@ -13,7 +29,7 @@ var functions = {
   func_add_line_numbers: {
     description: 'Add line numbers and all necessary spaces between numbers and text for proper alignment',
     call: function(text) {
-      lines = text.split(/\n/);
+      var lines = text.split(/\n/);
       var digits = Math.ceil(Math.log(lines.length) / Math.LN10);
       for (var i = 0; i < lines.length; i++)
       {
@@ -23,6 +39,41 @@ var functions = {
         lines[i] = prefix + lines[i];
       }
       return lines.join('\n');
+    }
+  },
+  func_add_date_mark: {
+    description: 'Append a time-tracking block for the current day',
+    call: function(text) {
+      var blocks = text.split(/\n\n/);
+      var date = new Date();
+      var date_mark = $.sprintf('%04d-%02d-%02d', date.getYear() + 1900, date.getMonth() + 1, date.getDate());
+      return text + '\n' + date_mark + '\n';
+    }
+  },
+  func_toggle_status: {
+    description: 'Toggle status, i.e. add start mark when idle, add stop mark when working',
+    call: function(text) {
+      var lines = text.split(/\n/);
+      var date = new Date();
+      var mark = $.sprintf('%02d%02d', date.getHours(), date.getMinutes());
+      mark += lines[lines.length - 1].match(/^\d+-$/) ? '\n' : '-';
+      return text + mark;
+    }
+  },
+  func_calculcate_sums: {
+    description: 'Calculate daily sums and put them in parentheses next to the date marks',
+    call: function(text) {
+      var blocks = text.split(/\n\n/);
+      for (var i = 0; i < blocks.length; i++)
+      {
+        var minutes = 0;
+        var block_lines = blocks[i].split(/\n/);
+        for (var j = 1; j < block_lines.length; j++)
+          minutes += stx.duration(block_lines[j]);
+        block_lines[0] += ' (' + stx.hhmm(minutes) + ')';
+        blocks[i] = block_lines.join('\n');
+      }
+      return blocks.join('\n\n');
     }
   }
 };
